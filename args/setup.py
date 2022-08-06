f9a7cd4d2b66624888a2eff3effb54ca9d761434
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-08-06 19:26:02 +0800

    initial

--- args/setup.py
+++ args/setup.py
@@ -26,7 +26,7 @@ from args.dataset import (
     dataset_args,
     linear_augmentations_args,
 )
-from args.utils import additional_setup_linear, additional_setup_pretrain
+from args.utils import additional_setup_linear
 from utils.auto_resumer import AutoResumer
 from utils.checkpointer import Checkpointer
 
@@ -46,62 +46,6 @@ else:
     _dali_available = True
 
 
-def parse_args_pretrain() -> argparse.Namespace:
-    """Parses dataset, augmentation, pytorch lightning, model specific and additional args.
-
-    First adds shared args such as dataset, augmentation and pytorch lightning args, then pulls the
-    model name from the command and proceeds to add model specific args from the desired class. If
-    wandb is enabled, it adds checkpointer args. Finally, adds additional non-user given parameters.
-
-    Returns:
-        argparse.Namespace: a namespace containing all args needed for pretraining.
-    """
-
-    parser = argparse.ArgumentParser()
-
-    # add shared arguments
-    dataset_args(parser)
-    augmentations_args(parser)
-    custom_dataset_args(parser)
-
-    # add pytorch lightning trainer args
-    parser = pl.Trainer.add_argparse_args(parser)
-
-    # add method-specific arguments
-    parser.add_argument("--method", type=str)
-
-    # THIS LINE IS KEY TO PULL THE MODEL NAME
-    temp_args, _ = parser.parse_known_args()
-
-    # add model specific args
-    parser = METHODS[temp_args.method].add_model_specific_args(parser)
-
-    # add auto checkpoint/umap args
-    parser.add_argument("--save_checkpoint", action="store_true")
-    parser.add_argument("--auto_umap", action="store_true")
-    parser.add_argument("--auto_resume", action="store_true")
-    temp_args, _ = parser.parse_known_args()
-
-    # optionally add checkpointer and AutoUMAP args
-    if temp_args.save_checkpoint:
-        parser = Checkpointer.add_checkpointer_args(parser)
-
-    if _umap_available and temp_args.auto_umap:
-        parser = AutoUMAP.add_auto_umap_args(parser)
-
-    if temp_args.auto_resume:
-        parser = AutoResumer.add_autoresumer_args(parser)
-
-    if _dali_available and temp_args.data_format == "dali":
-        parser = PretrainDALIDataModule.add_dali_args(parser)
-
-    # parse args
-    args = parser.parse_args()
-
-    # prepare arguments with additional setup
-    additional_setup_pretrain(args)
-
-    return args
 
 
 def parse_args_linear() -> argparse.Namespace:
@@ -153,54 +97,3 @@ def parse_args_linear() -> argparse.Namespace:
 
     return args
 
-
-def parse_args_knn() -> argparse.Namespace:
-    """Parses arguments for offline K-NN.
-
-    Returns:
-        argparse.Namespace: a namespace containing all args needed for pretraining.
-    """
-
-    parser = argparse.ArgumentParser()
-
-    # add knn args
-    parser.add_argument("--pretrained_checkpoint_dir", type=str)
-    parser.add_argument("--batch_size", type=int, default=16)
-    parser.add_argument("--num_workers", type=int, default=10)
-    parser.add_argument("--k", type=int, nargs="+")
-    parser.add_argument("--temperature", type=float, nargs="+")
-    parser.add_argument("--distance_function", type=str, nargs="+")
-    parser.add_argument("--feature_type", type=str, nargs="+")
-
-    # add shared arguments
-    dataset_args(parser)
-    custom_dataset_args(parser)
-
-    # parse args
-    args = parser.parse_args()
-
-    return args
-
-
-def parse_args_umap() -> argparse.Namespace:
-    """Parses arguments for offline UMAP.
-
-    Returns:
-        argparse.Namespace: a namespace containing all args needed for pretraining.
-    """
-
-    parser = argparse.ArgumentParser()
-
-    # add knn args
-    parser.add_argument("--pretrained_checkpoint_dir", type=str)
-    parser.add_argument("--batch_size", type=int, default=16)
-    parser.add_argument("--num_workers", type=int, default=10)
-
-    # add shared arguments
-    dataset_args(parser)
-    custom_dataset_args(parser)
-
-    # parse args
-    args = parser.parse_args()
-
-    return args
