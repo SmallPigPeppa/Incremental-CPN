6a83d282db05580e2d7d29f6793c55cc77ca82f8
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-08-07 00:39:16 +0800

    initial

--- scripts/joint_train_cifar_cpn.sh
+++ scripts/joint_train_cifar_cpn.sh
@@ -2,6 +2,7 @@ data_path=/mnt/mmtech01/usr/liuwenzhuo/torch_ds
 pretrained_dir=/mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/simclr/2mv95572
 pretrained_path="$(ls $pretrained_dir/*.ckpt)"
 echo "pretrained_path: $pretrained_path"
+pl_lambda=0.2
 /share/wenzhuoliu/conda-envs/solo-learn/bin/python joint_train.py \
     --dataset cifar100 \
     --encoder resnet50 \
@@ -20,14 +21,14 @@ echo "pretrained_path: $pretrained_path"
     --batch_size 256 \
     --num_workers 10 \
     --crop_size 32 \
-    --name joint-train-cifar-cpn \
+    --name joint-train-cifar-cpn-lamda:$pl_lambda \
     --pretrained_feature_extractor ${pretrained_path} \
     --project Incremental-CPN \
     --entity pigpeppa \
     --wandb \
     --save_checkpoint \
     --auto_resume \
-    --pl_lambda 0.2 \
+    --pl_lambda $pl_lambda \
     --mean 0.5071 0.4867 0.4408 \
     --std 0.2675 0.2565 0.2761
 
