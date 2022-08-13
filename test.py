24f87b92afcd6164e30947b68a2194e1c5e2cba0
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-08-13 22:35:30 +0800

    initial

--- test.py
+++ test.py
@@ -118,8 +118,8 @@ if __name__=='__main__':
     GPUS = [0]
     BS0 = 128
     BS2 = 512
-    ckpt_dir='/mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/simclr/2mv95572'
-    # ckpt_dir='/share/wenzhuoliu/code/solo-learn/trained_models/byol/t3pmk238'
+    # ckpt_dir='/mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/simclr/2mv95572'
+    ckpt_dir='/share/wenzhuoliu/code/solo-learn/trained_models/byol/t3pmk238'
     # ckpt_dir='/share/wenzhuoliu/code/solo-learn/trained_models/mocov2plus/1kguyx5e'
     # ckpt_dir='/share/wenzhuoliu/code/solo-learn/trained_models/barlow_twins/1ehqqmug'
 
