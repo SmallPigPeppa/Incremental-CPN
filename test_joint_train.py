5ce77c1ed0f2fe7f30a7c62962be502dbb46ec2b
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-08-24 23:43:23 +0800

    initial

--- test_joint_train.py
+++ test_joint_train.py
@@ -153,8 +153,8 @@ if __name__ == '__main__':
     cifar_pipeline = {
         "T_train": transforms.Compose(
             [
-                transforms.ToTensor(),
                 transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
                 transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
             ]
         ),
