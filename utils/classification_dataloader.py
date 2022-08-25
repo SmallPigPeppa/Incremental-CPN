1b9c85782bc19e2bd8a012e5278690e373c0bfdf
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-08-25 21:32:38 +0800

    initial

--- utils/classification_dataloader.py
+++ utils/classification_dataloader.py
@@ -39,7 +39,7 @@ else:
 
 def split_dataset(dataset: Dataset, task_idx: List[int], tasks: list = None):
     assert len(dataset.classes) == sum([len(t) for t in tasks])
-    current_task = torch.tensor([tasks[i] for i in task_idx])
+    current_task =[tasks[i] for i in task_idx]
     print(tasks[0])
     print(current_task)
     mask = [(c in current_task) for c in dataset.targets]
