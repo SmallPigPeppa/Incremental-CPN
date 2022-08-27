dc602a2f0f8d7eb58993424fda211bccb7766b35
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-08-27 22:35:36 +0800

    initial

--- run_continual.sh
+++ run_continual.sh
@@ -13,7 +13,7 @@ do
       --train_data_path ${data_path} \
       --val_data_path ${data_path} \
       --max_epochs 100 \
-      --devices 1 \
+      --devices 2 \
       --accelerator gpu \
       --precision 16 \
       --optimizer sgd \
