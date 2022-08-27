ca7bb0a12a051890c600e715b05313aa8e952220
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-08-27 17:11:30 +0800

    initial

--- scripts/cpn_joint_cifar.sh
+++ scripts/cpn_joint_cifar.sh
@@ -1,5 +1,6 @@
-data_path=/mnt/mmtech01/usr/liuwenzhuo/torch_ds
-pretrained_dir=/mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/simclr/2mv95572
+data_path=/share/wenzhuoliu/torch_ds
+#pretrained_dir=/mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/simclr/2mv95572
+pretrained_dir=/share/wenzhuoliu/code/solo-learn/trained_models/swav/yaaves5o
 pretrained_path="$(ls $pretrained_dir/*.ckpt)"
 echo "pretrained_path: $pretrained_path"
 pl_lambda=0.03
