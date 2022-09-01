4ce1ed389b9353f87ab5e9df99ba54592f83f7d6
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-09-01 22:27:05 +0800

    initial

---
+++ run_all.sh
@@ -0,0 +1,6 @@
+. scripts/barlow_twins_cifar.sh
+. scripts/byol_cifar.sh
+. scripts/mocov2_cifar.sh
+. scripts/simclr_cifar.sh
+. scripts/simsiam_cifar.sh
+. scripts/swav_cifar.sh
--- scripts/byol_cifar.sh
+++ scripts/byol_cifar.sh
@@ -2,8 +2,7 @@ conda activate torch
 python main_continual.py \
       --num_tasks 5 \
       --pretrained_model /share/wenzhuoliu/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
-      --pretrained_method byol \
-      --project Incremental-CPN-v8.0-2
+      --pretrained_method byol
 
 python main_continual.py \
       --num_tasks 10 \
