28d0efbde7ea7656394e4fc50d2161b0ab3e42b8
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-09-02 16:09:26 +0800

    initial

--- scripts/simsiam_cifar.sh
+++ scripts/simsiam_cifar.sh
@@ -1,13 +1,13 @@
 conda activate torch
 python main_continual.py \
       --num_tasks 5 \
-      --pretrained_model /share/wenzhuoliu/code/solo-learn/trained_models/simsiam/22bn8hmt/simsiam-imagenet32-22bn8hmt-ep=999.ckpt \
+      --pretrained_model /share/wenzhuoliu/code/solo-learn/trained_models/simsiam/22bn8hmt/simsiam-imagenet32-22bn8hmt-ep=1000.ckpt \
       --pretrained_method simsiam
 
 
 python main_continual.py \
       --num_tasks 10 \
-      --pretrained_model /share/wenzhuoliu/code/solo-learn/trained_models/simsiam/22bn8hmt/simsiam-imagenet32-22bn8hmt-ep=999.ckpt \
+      --pretrained_model /share/wenzhuoliu/code/solo-learn/trained_models/simsiam/22bn8hmt/simsiam-imagenet32-22bn8hmt-ep=1000.ckpt \
       --pretrained_method simsiam \
       --cpn_initial means
 
@@ -16,7 +16,7 @@ python main_continual.py \
 
 CUDA_VISIBLE_DEVICES=4,5 python main_continual.py \
       --num_tasks 5 \
-      --pretrained_model /share/wenzhuoliu/code/solo-learn/trained_models/simsiam/22bn8hmt/simsiam-imagenet32-22bn8hmt-ep=999.ckpt \
+      --pretrained_model /share/wenzhuoliu/code/solo-learn/trained_models/simsiam/22bn8hmt/simsiam-imagenet32-22bn8hmt-ep=1000.ckpt \
       --pretrained_method simsiam \
       --cpn_initial means \
       --perfix means-initial-
