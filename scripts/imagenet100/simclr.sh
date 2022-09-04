94ad62b50a3df3910c4494926f6ee67d90e23d53
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-09-04 20:12:46 +0800

    initial

--- scripts/imagenet100/simclr.sh
+++ scripts/imagenet100/simclr.sh
@@ -1,5 +1,5 @@
 conda activate torch
-for lambda in 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.7 0.8
+for lambda in 0.01 0.025 0.05 0.075 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.7 0.8
 do
   CUDA_VISIBLE_DEVICES=4 python main_continual_debug.py \
         --num_tasks 5 \
