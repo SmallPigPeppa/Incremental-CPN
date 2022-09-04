4338fd2dbced1266a735c7a519d119e38ddf68da
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-09-04 10:20:18 +0800

    initial

--- scripts/imagenet100/simclr.sh
+++ scripts/imagenet100/simclr.sh
@@ -8,11 +8,11 @@ CUDA_VISIBLE_DEVICES=4,5 python main_continual.py \
       --dataset imagenet100 \
       --project Incremental-CPN-Imagenet100
 
-CUDA_VISIBLE_DEVICES=4,5 python main_continual.py \
-      --num_tasks 10 \
-      --pretrained_model /share/wenzhuoliu/code/ssl-pretrained-models/simclr_imagenet.ckpt \
-      --pretrained_method simclr \
-      --cpn_initial means \
-      --pl_lambda 0.3 \
-      --dataset imagenet100 \
-      --project Incremental-CPN-Imagenet100
+#CUDA_VISIBLE_DEVICES=4,5 python main_continual.py \
+#      --num_tasks 10 \
+#      --pretrained_model /share/wenzhuoliu/code/ssl-pretrained-models/simclr_imagenet.ckpt \
+#      --pretrained_method simclr \
+#      --cpn_initial means \
+#      --pl_lambda 0.3 \
+#      --dataset imagenet100 \
+#      --project Incremental-CPN-Imagenet100
