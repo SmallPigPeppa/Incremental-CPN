1e3813385f1c14b000ee7f0fc8738c69ac7893c0
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-09-06 20:46:31 +0800

    initial

---
+++ scripts/imagenet100/byol_latest.sh
@@ -0,0 +1,17 @@
+conda activate torch
+lambda=0.2
+pretrained_dir=/share/wenzhuoliu/code/ssl-pretrained-models/byol
+pretrained_path="$(ls $pretrained_dir/*.ckpt)"
+echo "pretrained_path: $pretrained_path"
+python main_continual.py \
+      --num_tasks 5 \
+      --pretrained_model $pretrained_path \
+      --pretrained_method byol \
+      --pl_lambda $lambda \
+      --dataset imagenet100 \
+      --project Incremental-CPN-Imagenet100
+
+#python main_continual.py \
+#      --num_tasks 10 \
+#      --pretrained_model /share/wenzhuoliu/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
+#      --pretrained_method byol
