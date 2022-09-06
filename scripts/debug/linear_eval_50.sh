4e7b77f5f8e7f9877081afe774553a93b01b2e9f
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-09-06 11:13:36 +0800

    initial

--- scripts/debug/linear_eval_50.sh
+++ scripts/debug/linear_eval_50.sh
@@ -1,5 +1,5 @@
 conda activate torch
-CUDA_VISIBLE_DEVICES=3 python main_joint_50.py \
+CUDA_VISIBLE_DEVICES=2,3 python main_joint_50.py \
       --num_tasks 5 \
       --pretrained_model /share/wenzhuoliu/code/ssl-pretrained-models/simclr_imagenet.ckpt \
       --pretrained_method simclr \
