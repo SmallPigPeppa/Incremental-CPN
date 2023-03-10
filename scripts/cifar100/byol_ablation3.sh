792786886e3ecc6964936cd8b7dc1aa98d6a2753
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-03-10 18:16:41 +0800

    initial

--- scripts/cifar100/byol_ablation3.sh
+++ scripts/cifar100/byol_ablation3.sh
@@ -1,7 +1,7 @@
 #conda activate torch
 
 
-python main_continual_linear.py \
+python main_continual_top.py \
       --num_tasks 1 \
       --pretrained_model /share/wenzhuoliu/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
       --pretrained_method byol\
