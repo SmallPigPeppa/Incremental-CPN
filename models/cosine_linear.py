740807afc85bd7b17923c7ff09928475f1065a0b
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-03-10 16:16:39 +0800

    initial

--- models/cosine_linear.py
+++ models/cosine_linear.py
@@ -5,29 +5,49 @@ from torch.nn import functional as F
 from pl_bolts.optimizers.lr_scheduler import LinearWarmupCosineAnnealingLR
 
 
-class IncrementalCPN(pl.LightningModule):
-    def __init__(self, dim_feature, num_classes, lr, epochs, warmup_epochs, lambda1, lambda2=0., **kwargs):
-        super(IncrementalCPN, self).__init__()
+
+class CosineLinear(nn.Module):
+    def __init__(self, in_features, out_features, bias=True):
+        super(CosineLinear, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = nn.Parameter(torch.Tensor(out_features, in_features))
+        if bias:
+            self.bias = nn.Parameter(torch.Tensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+
+    def forward(self, input):
+        cosine = nn.functional.cosine_similarity(input, self.weight, dim=1)
+        if self.bias is not None:
+            cosine = cosine + self.bias
+        return cosine
+
+
+class MLP(pl.LightningModule):
+    def __init__(self, dim_feature, num_classes, pl_lambda, lr, epochs, warmup_epochs, **kwargs):
+        super(MLP, self).__init__()
         self.dim_feature = dim_feature
         self.num_calsses = num_classes
-        self.lambda1 = lambda1
-        self.lambda2 = lambda2
+        self.pl_lambda = pl_lambda
         self.lr = lr
         self.epochs = epochs
         self.warmup_epochs = warmup_epochs
         self.extra_args = kwargs
-        self.prototypes = nn.ParameterList(
-            [nn.Parameter(torch.randn(1, self.dim_feature)) for i in range(num_classes)])
+        self.model = CosineLinear(dim_feature, num_classes)
 
     def task_initial(self, current_tasks, means=None):
-        if means is not None:
-            for i in current_tasks:
-                self.prototypes[i].data = torch.nn.Parameter((means[str(i)]).reshape(1, -1))
-        no_grad_idx = [i for i in range(self.num_calsses) if i not in current_tasks]
-        for i in no_grad_idx:
-            self.prototypes[i].requires_grad = False
-        for i in current_tasks:
-            self.prototypes[i].requires_grad = True
+        # if means is not None:
+        #     for i in current_tasks:
+        #         self.prototypes[i].data = torch.nn.Parameter((means[str(i)]).reshape(1, -1))1
+        # no_grad_idx = [i for i in range(self.num_calsses) if i not in current_tasks]
+        # for i in no_grad_idx:
+        #     self.prototypes[i].requires_grad = False
+        # for i in current_tasks:
+        #     self.prototypes[i].requires_grad = True
+        pass
 
     def configure_optimizers(self):
         optimizer = torch.optim.SGD(self.parameters(), lr=self.lr)
@@ -36,34 +56,18 @@ class IncrementalCPN(pl.LightningModule):
         return [optimizer], [scheduler]
 
     def forward(self, x):
-        x = x.reshape(-1, 1, self.dim_feature)
-        prototypes_list = [i for i in self.prototypes]
-        d = torch.pow(x - torch.cat(prototypes_list), 2)
-        d = torch.sum(d, dim=2)
-        return d
+        out = self.model(x)
+        return out
 
     def share_step(self, batch, batch_idx):
         x, targets = batch
-        d = self.forward(x)
-        logits = -1. * d
+        logits = self.forward(x)
         # ce loss
         ce_loss = F.cross_entropy(logits, targets)
-        # pl loss
-        pl_loss = torch.index_select(d, dim=1, index=targets)
-        pl_loss = torch.diagonal(pl_loss)
-        pl_loss = torch.mean(pl_loss)
-        # pl cosine loss
-        prototypes_list = [i for i in self.prototypes]
-        w = torch.cat(prototypes_list)
-        c = torch.index_select(w, dim=0, index=targets)
-        cos_matrix = F.normalize(x, p=2, dim=1) * F.normalize(c, p=2, dim=1)
-        pl_cos_loss = -1. * torch.sum(cos_matrix)
-        # all loss
-        loss = ce_loss + pl_loss * self.lambda1 + pl_cos_loss * self.lambda2
         # acc
         preds = torch.argmax(logits, dim=1)
         acc = torch.sum(preds == targets) / targets.shape[0]
-        return {"ce_loss": ce_loss, "pl_loss": pl_loss, "pl_cosine_loss": pl_cos_loss, "acc": acc, "loss": loss}
+        return {"acc": acc, "loss": ce_loss}
 
     def training_step(self, batch, batch_idx):
         out = self.share_step(batch, batch_idx)
--- scripts/cifar100/byol_ablation3.sh
+++ scripts/cifar100/byol_ablation3.sh
@@ -1,23 +1,45 @@
 #conda activate torch
 
 
-python main_continual_linear.py \
+#python main_continual_linear.py \
+#      --num_tasks 5 \
+#      --pretrained_model /share/wenzhuoliu/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
+#      --pretrained_method byol\
+#      --pl_lambda 0 \
+#      --project IPC-ablation-exp \
+#      --epochs 30 \
+#      --perfix linear_
+#
+#python main_continual_linear.py \
+#      --num_tasks 10 \
+#      --pretrained_model /share/wenzhuoliu/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
+#      --pretrained_method byol\
+#      --pl_lambda 0 \
+#      --project IPC-ablation-exp \
+#      --epochs 30 \
+#      --perfix linear_
+#
+
+
+
+
+python main_continual_cosine_linear.py \
       --num_tasks 5 \
       --pretrained_model /share/wenzhuoliu/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
       --pretrained_method byol\
       --pl_lambda 0 \
       --project IPC-ablation-exp \
       --epochs 30 \
-      --perfix linear_
+      --perfix cos_linear_
 
-python main_continual_linear.py \
+python main_continual_cosine_linear.py \
       --num_tasks 10 \
       --pretrained_model /share/wenzhuoliu/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
       --pretrained_method byol\
       --pl_lambda 0 \
       --project IPC-ablation-exp \
       --epochs 30 \
-      --perfix linear_
+      --perfix cos_linear_
 
 #python main_continual_pc.py \
 #      --num_tasks 5 \
