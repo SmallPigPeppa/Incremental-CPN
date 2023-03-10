4ef0369ed5a38d5ae12a9a364dc3fea1a0bac0de
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-03-10 16:24:19 +0800

    initial

--- scripts/cifar100/byol_ablation3.sh
+++ scripts/cifar100/byol_ablation3.sh
@@ -23,23 +23,23 @@
 
 
 #
-#python main_continual_cosine_linear.py \
-#      --num_tasks 5 \
-#      --pretrained_model /share/wenzhuoliu/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
-#      --pretrained_method byol\
-#      --pl_lambda 0 \
-#      --project IPC-ablation-exp \
-#      --epochs 30 \
-#      --perfix cos_linear_
-#
-#python main_continual_cosine_linear.py \
-#      --num_tasks 10 \
-#      --pretrained_model /share/wenzhuoliu/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
-#      --pretrained_method byol\
-#      --pl_lambda 0 \
-#      --project IPC-ablation-exp \
-#      --epochs 30 \
-#      --perfix cos_linear_
+python main_continual_cosine_linear.py \
+      --num_tasks 5 \
+      --pretrained_model /share/wenzhuoliu/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
+      --pretrained_method byol\
+      --pl_lambda 0 \
+      --project IPC-ablation-exp \
+      --epochs 30 \
+      --perfix cos_linear_
+
+python main_continual_cosine_linear.py \
+      --num_tasks 10 \
+      --pretrained_model /share/wenzhuoliu/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
+      --pretrained_method byol\
+      --pl_lambda 0 \
+      --project IPC-ablation-exp \
+      --epochs 30 \
+      --perfix cos_linear_
 
 
 
