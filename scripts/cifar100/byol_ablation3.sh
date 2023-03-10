4cc15f44993869c4a0aa2aa00d4e69380135daf4
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-03-10 15:32:48 +0800

    initial

---
+++ scripts/cifar100/byol_ablation3.sh
@@ -0,0 +1,42 @@
+#conda activate torch
+
+
+python main_continual_linear.py \
+      --num_tasks 5 \
+      --pretrained_model /share/wenzhuoliu/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
+      --pretrained_method byol\
+      --pl_lambda 0 \
+      --project IPC-ablation-exp \
+      --epochs 30 \
+      --cpn_initial means \
+      --perfix linear_
+
+python main_continual_pc.py \
+      --num_tasks 10 \
+      --pretrained_model /share/wenzhuoliu/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
+      --pretrained_method byol\
+      --pl_lambda 0 \
+      --project IPC-ablation-exp \
+      --epochs 30 \
+      --cpn_initial means \
+      --perfix linear_
+
+#python main_continual_pc.py \
+#      --num_tasks 5 \
+#      --pretrained_model /share/wenzhuoliu/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
+#      --pretrained_method byol\
+#      --pl_lambda 0.2 \
+#      --project IPC-ablation-exp \
+#      --epochs 30 \
+#      --cpn_initial means \
+#      --perfix pc_w_pl_
+#
+#python main_continual_pc.py \
+#      --num_tasks 10 \
+#      --pretrained_model /share/wenzhuoliu/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
+#      --pretrained_method byol\
+#      --pl_lambda 0.2 \
+#      --project IPC-ablation-exp \
+#      --epochs 30 \
+#      --cpn_initial means \
+#      --perfix pc_w_pl_
