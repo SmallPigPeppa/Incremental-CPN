fd353fe479e7d755e73627203793c6d1d6be4e56
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-03-20 21:45:36 +0800

    initial

--- run_cifar_good_10steps_byol_task_acc.sh
+++ run_cifar_good_10steps_byol_task_acc.sh
@@ -1,7 +1,7 @@
 #conda activate torch
 
 lambda=0.2
-CUDA_VISIBLE_DEVICES=0 python main_continual.py \
+CUDA_VISIBLE_DEVICES=0 python main_continual_task_acc.py \
       --num_tasks 10 \
       --pretrained_model /share/wenzhuoliu/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
       --pretrained_method byol \
