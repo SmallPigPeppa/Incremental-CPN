23545ec1949929fa0876611818c348218c678466
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-03-20 21:11:31 +0800

    initial

---
+++ run_cifar_good_10steps_byol_task_acc.sh
@@ -0,0 +1,11 @@
+conda activate torch
+
+lambda=0.2
+CUDA_VISIBLE_DEVICES=0 python main_continual.py \
+      --num_tasks 10 \
+      --pretrained_model /share/wenzhuoliu/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
+      --pretrained_method byol \
+      --cpn_initial means \
+      --pl_lambda $lambda \
+      --dataset cifar100 \
+      --project Incremental-CPN-cifar100-task-acc
