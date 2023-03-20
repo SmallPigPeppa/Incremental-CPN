3edda30a6680e6512f0c85b6bfb4bee97a1b07e2
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-03-20 22:49:03 +0800

    initial

--- run_cifar_good_10steps_byol_task_acc.sh
+++ run_cifar_good_10steps_byol_task_acc.sh
@@ -3,6 +3,7 @@
 lambda=0.2
 CUDA_VISIBLE_DEVICES=0 python main_continual_task_acc.py \
       --num_tasks 10 \
+      --epochs 1 \
       --pretrained_model /share/wenzhuoliu/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
       --pretrained_method byol \
       --cpn_initial means \
