787dec209bf4dfe44b20ed573d339bc3dc150178
lwz <905328134@qq.com>
2024-04-28 20:12:53 +0800

    initial

---
+++ scripts/cifar100-rebuttal/Joint.sh
@@ -0,0 +1,16 @@
+conda activate torch
+python main_continual_linear_joint.py \
+      --num_tasks 5 \
+      --pretrained_model /ppio_net0/pretrained/byol-imagenet32-t3pmk238-ep=999.ckpt \
+      --pretrained_method byol \
+      --data_path /ppio_net0/torch_ds \
+      --dataset cifar100 \
+      --project PR-rebuttal
+
+python main_continual_linear_joint.py \
+      --num_tasks 10 \
+      --pretrained_model /ppio_net0/pretrained/byol-imagenet32-t3pmk238-ep=999.ckpt \
+      --pretrained_method byol \
+      --data_path /ppio_net0/torch_ds \
+      --dataset cifar100 \
+      --project PR-rebuttal
