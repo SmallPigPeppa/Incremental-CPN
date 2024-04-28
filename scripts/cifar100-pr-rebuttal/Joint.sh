8236379745a6f55f255a39432a3e49e82165888e
lwz <905328134@qq.com>
2024-04-28 20:30:59 +0800

    initial

--- scripts/cifar100-pr-rebuttal/Joint.sh
+++ scripts/cifar100-pr-rebuttal/Joint.sh
@@ -6,6 +6,32 @@ python main_continual_linear_joint.py \
       --dataset cifar100 \
       --project PR-rebuttal
 
+python main_continual_linear_joint.py \
+      --num_tasks 10 \
+      --pretrained_model /ppio_net0/pretrained/byol-imagenet32-t3pmk238-ep=999.ckpt \
+      --pretrained_method byol \
+      --data_path /ppio_net0/torch_ds \
+      --dataset cifar100 \
+      --project PR-rebuttal
+
+
+python main_continual_linear_joint.py \
+      --num_tasks 5 \
+      --pretrained_model /ppio_net0/pretrained/byol-imagenet-3tx0at58-ep=999.ckpt \
+      --pretrained_method byol \
+      --data_path /ppio_net0/torch_ds \
+      --dataset imagenet100 \
+      --project PR-rebuttal
+
+python main_continual_linear_joint.py \
+      --num_tasks 10 \
+      --pretrained_model /ppio_net0/pretrained/byol-imagenet-3tx0at58-ep=999.ckpt \
+      --pretrained_method byol \
+      --data_path /ppio_net0/torch_ds \
+      --dataset imagenet100 \
+      --project PR-rebuttal
+
+
 #python main_continual_linear_joint.py \
 #      --num_tasks 10 \
 #      --pretrained_model /ppio_net0/pretrained/byol-imagenet32-t3pmk238-ep=999.ckpt \
