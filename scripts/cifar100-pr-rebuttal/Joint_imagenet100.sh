a4130e99569c0e6c89bd890b8d8e2d8abab0cf60
lwz <905328134@qq.com>
2024-04-29 01:09:28 +0800

    initial

--- scripts/cifar100-pr-rebuttal/Joint_imagenet100.sh
+++ scripts/cifar100-pr-rebuttal/Joint_imagenet100.sh
@@ -1,16 +1,16 @@
-python pr_rebuttal_joint.py \
-      --num_tasks 5 \
-      --pretrained_model /ppio_net0/pretrained/byol-imagenet-3tx0at58-ep=999.ckpt \
-      --pretrained_method byol \
-      --data_path /ppio_net0/torch_ds \
-      --dataset imagenet100 \
-      --epochs 30 \
-      --lr 0.1 \
-      --batch_size 256 \
-      --project PR-rebuttal
+#python pr_rebuttal_joint.py \
+#      --num_tasks 5 \
+#      --pretrained_model /ppio_net0/pretrained/byol-imagenet-3tx0at58-ep=999.ckpt \
+#      --pretrained_method byol \
+#      --data_path /ppio_net0/torch_ds \
+#      --dataset imagenet100 \
+#      --epochs 30 \
+#      --lr 0.1 \
+#      --batch_size 256 \
+#      --project PR-rebuttal
 
 
-python pr_rebuttal_joint.py \
+python pr_rebuttal_joint_linear.py \
       --num_tasks 10 \
       --pretrained_model /ppio_net0/pretrained/byol-imagenet-3tx0at58-ep=999.ckpt \
       --pretrained_method byol \
@@ -20,6 +20,3 @@ python pr_rebuttal_joint.py \
       --lr 0.1 \
       --batch_size 256 \
       --project PR-rebuttal
-
-
-python /ppio_net0/code/openapi.py stop eb3d0a48c30d3cd7
