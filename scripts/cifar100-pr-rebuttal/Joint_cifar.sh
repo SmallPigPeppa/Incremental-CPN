fbe757ac434375511fc51bffdafbde2317fa20c6
lwz <905328134@qq.com>
2024-04-28 22:29:13 +0800

    initial

---
+++ scripts/cifar100-pr-rebuttal/Joint_cifar.sh
@@ -0,0 +1,23 @@
+python pr_rebuttal_joint_ipc.py \
+      --num_tasks 5 \
+      --pretrained_model /ppio_net0/pretrained/byol-imagenet32-t3pmk238-ep=999.ckpt \
+      --pretrained_method byol \
+      --data_path /ppio_net0/torch_ds \
+      --dataset cifar100 \
+      --epochs 30 \
+      --lr 0.1 \
+      --batch_size 256 \
+      --project PR-rebuttal
+
+python pr_rebuttal_joint_ipc.py \
+      --num_tasks 10 \
+      --pretrained_model /ppio_net0/pretrained/byol-imagenet32-t3pmk238-ep=999.ckpt \
+      --pretrained_method byol \
+      --data_path /ppio_net0/torch_ds \
+      --dataset cifar100 \
+      --epochs 30 \
+      --lr 0.1 \
+      --batch_size 256 \
+      --project PR-rebuttal
+
+#python /ppio_net0/code/openapi.py stop afffe4aba431b52b
