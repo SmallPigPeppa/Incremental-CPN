04fdcba39b766e3f1a626f8a56ad612fa51a93cd
lwz <905328134@qq.com>
2024-04-28 20:48:22 +0800

    initial

--- scripts/cifar100-pr-rebuttal/Joint.sh
+++ scripts/cifar100-pr-rebuttal/Joint.sh
@@ -4,33 +4,37 @@ python main_continual_linear_joint.py \
       --pretrained_method byol \
       --data_path /ppio_net0/torch_ds \
       --dataset cifar100 \
+      --epochs 80 \
       --project PR-rebuttal
 
-python main_continual_linear_joint.py \
-      --num_tasks 10 \
-      --pretrained_model /ppio_net0/pretrained/byol-imagenet32-t3pmk238-ep=999.ckpt \
-      --pretrained_method byol \
-      --data_path /ppio_net0/torch_ds \
-      --dataset cifar100 \
-      --project PR-rebuttal
-
-
-python main_continual_linear_joint.py \
-      --num_tasks 5 \
-      --pretrained_model /ppio_net0/pretrained/byol-imagenet-3tx0at58-ep=999.ckpt \
-      --pretrained_method byol \
-      --data_path /ppio_net0/torch_ds \
-      --dataset imagenet100 \
-      --project PR-rebuttal
-
-
-python main_continual_linear_joint.py \
-      --num_tasks 10 \
-      --pretrained_model /ppio_net0/pretrained/byol-imagenet-3tx0at58-ep=999.ckpt \
-      --pretrained_method byol \
-      --data_path /ppio_net0/torch_ds \
-      --dataset imagenet100 \
-      --project PR-rebuttal
+#python main_continual_linear_joint.py \
+#      --num_tasks 10 \
+#      --pretrained_model /ppio_net0/pretrained/byol-imagenet32-t3pmk238-ep=999.ckpt \
+#      --pretrained_method byol \
+#      --data_path /ppio_net0/torch_ds \
+#      --dataset cifar100 \
+#      --epochs 80 \
+#      --project PR-rebuttal
+#
+#
+#python main_continual_linear_joint.py \
+#      --num_tasks 5 \
+#      --pretrained_model /ppio_net0/pretrained/byol-imagenet-3tx0at58-ep=999.ckpt \
+#      --pretrained_method byol \
+#      --data_path /ppio_net0/torch_ds \
+#      --dataset imagenet100 \
+#      --epochs 80 \
+#      --project PR-rebuttal
+#
+#
+#python main_continual_linear_joint.py \
+#      --num_tasks 10 \
+#      --pretrained_model /ppio_net0/pretrained/byol-imagenet-3tx0at58-ep=999.ckpt \
+#      --pretrained_method byol \
+#      --data_path /ppio_net0/torch_ds \
+#      --dataset imagenet100 \
+#      --epochs 80 \
+#      --project PR-rebuttal
 
 
 #python main_continual_linear_joint.py \
